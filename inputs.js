9e785df80220316e8e8e1dbbed1adc3da0b7c953
Ece ÇANGA <44345414+ececanga@users.noreply.github.com>
2021-07-02 16:12:42 +0300

    Button visibility updated, title added

--- inputs.js
+++ inputs.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, StyleSheet, ScrollView } from 'react-native'
+import ScrollViewCommands from 'react-native/Libraries/Components/ScrollView/ScrollViewCommands';
 
 class Inputs extends Component {
     state = {
@@ -29,56 +30,74 @@ class Inputs extends Component {
         this.setState({ sideEffect: text })
     }
 
-    login = (email, pass) => {
-        alert('email: ' + email + ' password: ' + pass)
+    login = (name, birthday, city, gender, vaccineType, sideEffect) => {
+        alert('name: ' + name + ' birthday: ' + birthday
+                + 'city: ' + city + 'gender: ' + gender
+                + 'vaccineType: ' + vaccineType + 'sideEffect: ' + sideEffect)
     }
+
+    isFormValid = () => {
+        const {name, birthday, city, gender, vaccineType, sideEffect} = this.state;
+        return name && birthday && city && gender && vaccineType && sideEffect;
+    }
+
     render() {
         return (
             <View style = {styles.container}>
-                <TextInput style = {styles.input}
-                           underlineColorAndroid = "transparent"
-                           placeholder = "○ Name-surname"
-                           placeholderTextColor = "#9a73ef"
-                           autoCapitalize = "none"
-                           onChangeText = {this.handleName}/>
-                <TextInput style = {styles.input}
-                           underlineColorAndroid = "transparent"
-                           placeholder = "○ Birth Date"
-                           placeholderTextColor = "#9a73ef"
-                           autoCapitalize = "none"
-                           onChangeText = {this.handleBirthday}/>
-                <TextInput style = {styles.input}
-                           underlineColorAndroid = "transparent"
-                           placeholder = "○ City"
-                           placeholderTextColor = "#9a73ef"
-                           autoCapitalize = "none"
-                           onChangeText = {this.handleCity}/>
-                <TextInput style = {styles.input}
-                           underlineColorAndroid = "transparent"
-                           placeholder = "○ Gender"
-                           placeholderTextColor = "#9a73ef"
-                           autoCapitalize = "none"
-                           onChangeText = {this.handleGender}/>
-                <TextInput style = {styles.input}
-                           underlineColorAndroid = "transparent"
-                           placeholder = "○ Vaccine type they applied"
-                           placeholderTextColor = "#9a73ef"
-                           autoCapitalize = "none"
-                           onChangeText = {this.handleVaccineType}/>
-                <TextInput style = {styles.input}
-                           underlineColorAndroid = "transparent"
-                           placeholder = "○ Any side effect after vaccination"
-                           placeholderTextColor = "#9a73ef"
-                           autoCapitalize = "none"
-                           onChangeText = {this.handleVaccineType}/>
+                <ScrollView>
+                    <Text style={styles.titleStyle}>COVID-19 VACCINE SURVEY FORM</Text>
+                    <TextInput style = {styles.input}
+                               underlineColorAndroid = "transparent"
+                               placeholder = "○ Name-surname"
+                               placeholderTextColor = "#8e9031"
+                               autoCapitalize = "none"
+                               onChangeText = {this.handleName}/>
+                    <TextInput style = {styles.input}
+                               underlineColorAndroid = "transparent"
+                               placeholder = "○ Birth Date"
+                               placeholderTextColor = "#8e9031"
+                               autoCapitalize = "none"
+                               onChangeText = {this.handleBirthday}/>
+                    <TextInput style = {styles.input}
+                               underlineColorAndroid = "transparent"
+                               placeholder = "○ City"
+                               placeholderTextColor = "#8e9031"
+                               autoCapitalize = "none"
+                               onChangeText = {this.handleCity}/>
+                    <TextInput style = {styles.input}
+                               underlineColorAndroid = "transparent"
+                               placeholder = "○ Gender"
+                               placeholderTextColor = "#8e9031"
+                               autoCapitalize = "none"
+                               onChangeText = {this.handleGender}/>
+                    <TextInput style = {styles.input}
+                               underlineColorAndroid = "transparent"
+                               placeholder = "○ Vaccine type they applied"
+                               placeholderTextColor = "#8e9031"
+                               autoCapitalize = "none"
+                               onChangeText = {this.handleVaccineType}/>
+                    <TextInput style = {styles.input}
+                               underlineColorAndroid = "transparent"
+                               placeholder = "○ Any side effect after vaccination"
+                               placeholderTextColor = "#8e9031"
+                               autoCapitalize = "none"
+                               onChangeText = {this.handleSideEffect}/>
 
-                <TouchableOpacity
-                    style = {styles.submitButton}
-                    onPress = {
-                        () => this.login(this.state.email, this.state.password)
-                    }>
-                    <Text style = {styles.submitButtonText}> Send </Text>
-                </TouchableOpacity>
+
+                    {
+                        !this.isFormValid() ? null : (
+                        <View>
+                            <TouchableOpacity
+                                disabled={!this.isFormValid}
+                                style = {styles.submitButton}
+                                onPress = {
+                                    () => this.login(this.state.name, this.state.birthday, this.state.city, this.state.gender, this.state.vaccineType, this.state.sideEffect)
+                                }>
+                                <Text style = {styles.submitButtonText}> Send </Text>
+                            </TouchableOpacity>
+                        </View>)
+                    }
+                </ScrollView>
             </View>
         )
     }
@@ -87,21 +106,30 @@ export default Inputs
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 23
+        paddingTop: 23,
     },
     input: {
         margin: 15,
-        height: 40,
-        borderColor: '#7a42f4',
+        height: 50,
+        borderColor: '#8e9031',
         borderWidth: 1
     },
     submitButton: {
-        backgroundColor: '#7a42f4',
+        backgroundColor: '#8e9031',
         padding: 10,
         margin: 15,
         height: 40,
     },
     submitButtonText:{
-        color: 'white'
-    }
+        color: 'white',
+        textAlign: "center",
+    },
+    titleStyle:{
+        //textDecorationColor: "#ffffff",
+        color: "#8e9031",
+        fontSize: 36,
+        alignSelf: "center",
+        textAlign: "center",
+        //fontFamily: "monospace",
+    },
 })
